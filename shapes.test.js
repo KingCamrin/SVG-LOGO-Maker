379ea0ecbcd103d39bef98cd8ef77e4375a72361
Cam <141684224+KingCamrin@users.noreply.github.com>
2024-05-15 21:59:03 -0500

    Create shapes.test.js

---
+++ shapes.test.js
@@ -0,0 +1,31 @@
+const { Circle, Square, Triangle } = require('./Lib/shape');
+
+describe('Circle', () => {
+    test("circle should be a purple circle", () => {
+        const testCirle = `<circle cx="150" cy="100" r="80" fill="green" />`;
+        const circle = new Circle();
+        circle.setColor('green');
+        const actualCircle = circle.render();
+        expect(actualCircle).toEqual(testCirle);
+    })
+    });
+
+describe('Square', () => {
+    test("square should be a red square", () => {
+        const testSquare = `<rect x="90" y="40" width="130" height="110" fill="yellow" />`;
+        const square = new Square();
+        square.setColor('yellow');
+        const actualSquare = square.render();
+        expect(actualSquare).toEqual(testSquare);
+    })
+    });
+
+describe('Triangle', () => {
+    test("triangle should be a green triangle", () => {
+        const testTriangle = `<polygon points="150,18 244,182 56,182" fill="purple" />`;
+        const triangle = new Triangle();
+        triangle.setColor('purple');
+        const actualTriangle = triangle.render();
+        expect(actualTriangle).toEqual(testTriangle);
+    })
+    });
