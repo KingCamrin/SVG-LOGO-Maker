c9950c3578a26e47c02655874e135fdf49642886
KingCamrin <camrindavis58@gmail.com>
2024-05-15 21:37:48 -0500

    Refactor CLI module imports and remove unused code

--- svg.js
+++ svg.js
@@ -1,24 +1,21 @@
 class SVG {
   constructor() {
-    this.textElement = "";
-    this.shapeElement = "";
+      this.shape = "";
+      this.text = "";
   }
 
   render() {
-    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shapeElement}${this.textElement}</svg>`;
+      return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${this.shape} ${this.text} </svg>`;
   }
 
-  setText(message, color) {
-    if (message.length > 3) {
-      throw new Error("Text must not exceed 3 characters.");
-    }
-    this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${message}</text>`;
+  setText(userText, userColor) {
+      this.text = `<text x="50%" y="57%" font-size="40" text-anchor="middle" fill="${userColor}">${userText}</text>`;
   }
 
-  setShape(shape) {
-    this.shapeElement = shape.render();
+
+  setShape(userShape) {
+      this.shape = userShape.render();
   }
 }
-
-
 module.exports = SVG;
